d451fac1587d2bd4bf19644ccbd80296faf13078
Lewis <lewisbirch2@googlemail.com>
2025-05-23 14:43:03 +0100

    Basic prints. Learning syntax

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(123456789);
+
+    //Variables must be declared as tehy are initialised. Variable types include String, int, boolean, etc... 
+        String name = "Lewis";
+        System.out.println(name);
+
+        int number = 10;
+        System.out.println(number);
+
+        //Variables can be loosely assigned, and defined later. See below. 
+        int letNum;
+        letNum = 15;
+        System.out.println(letNum);
+
+        // things can also be changed later, without 'let' needed. See below.
+        number = 20;
+        System.out.println(number);
     }}
 
     // javac Main.java
